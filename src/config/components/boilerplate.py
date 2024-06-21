8c088c42efce015581564331cdf2405637139248
OleksandrYanchar <contact.oleksandryanchar@gmail.com>
2024-06-21 13:40:50 +0300

    UPDATE

--- src/config/components/boilerplate.py
+++ src/config/components/boilerplate.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+TEST_DISCOVERY_ROOT = BASE_DIR
+
 ROOT_URLCONF = "config.urls"
 
 WSGI_APPLICATION = "config.wsgi.application"
